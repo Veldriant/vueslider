33e8bbc85ee8c98197c4cdbd4f44a2fcf61f985d
Veldriant <65666734+Veldriant@users.noreply.github.com>
2023-04-17 17:32:15 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <productCard msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import productCard from './components/productCard.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
     productCard
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
